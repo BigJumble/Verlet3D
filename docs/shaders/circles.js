fc3b4b36615c6ea28bb06016c956bf63e7d3869a
Eimantas <eima.eima1244@gmail.com>
2025-01-19 00:38:29 +0200

    10mln

--- docs/shaders/circles.js
+++ docs/shaders/circles.js
@@ -247,4 +247,4 @@ export class CameraShader {
     }
 }
 CameraShader.lightDirection = new Float32Array([0.0, -1.0, 0.0]); // Light direction in world space
-CameraShader.NUM_INSTANCES = 1000000;
+CameraShader.NUM_INSTANCES = 10000000;
